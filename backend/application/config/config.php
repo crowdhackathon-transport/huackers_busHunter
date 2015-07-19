018c08731b8b6e13e6930cc9cfabe33db088bd4e
Giannis Meletakis <meletakis@gmail.com>
2015-07-19 19:26:14 +0300

    Update config.php

--- backend/application/config/config.php
+++ backend/application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://83.212.116.159/trans';
+$config['base_url'] = 'xxxxxxxx/xxxxxx/';
 
 /*
 |--------------------------------------------------------------------------
